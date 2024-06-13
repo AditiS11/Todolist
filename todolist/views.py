02447c3ba1b5e49a6c19445d60afd6efe7dd221b
Aditi Srinivas <aditisrinivas@Aditis-MacBook-Air.local>
2024-06-13 08:44:20 +0530

    Displaying the data from the database

--- todolist/views.py
+++ todolist/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Todolist
 
 # Create your views here.
 def index(request):
-    return render(request,'todolist/index.html')
-    
+
+    todo_item = Todolist.objects.order_by('id') #Creating a variable to hold all the objects from the database that matches and todolist and store it inside the context as a list and furthur use it on our template
+    context={'todo_items' : todo_item}
+    return render(request,'todolist/index.html',context) #The context is passed to the template where it can be used
